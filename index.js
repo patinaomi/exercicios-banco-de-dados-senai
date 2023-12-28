34aabb2a6a99820028e422288fea0924fef38623
Patricia Naomi <patricianaomiyama@gmail.com>
2023-12-28 18:18:03 -0300

    trabalhando com listas

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('Trabalhando com listas');
+
+const listaDeDestinos = new Array(
+    'Salvador',
+    'Sao Paulo',
+    'Rio de Janeiro'
+);
+
+listaDeDestinos.push('Curitiba');
+
+console.log(listaDeDestinos);
+
+listaDeDestinos.splice(1,1);
